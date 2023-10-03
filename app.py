ee4620e71183d2469deaf444d7e7e28ffb03d3b7
mostafa Amer <73859325+Mostafa-ammer@users.noreply.github.com>
2023-10-03 18:55:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,20 +8,38 @@ from sklearn.metrics.pairwise import linear_kernel
 from surprise import Dataset, Reader, SVD
 from surprise.model_selection import train_test_split
 
+# Set Streamlit app title and add some custom CSS for styling
+st.markdown(
+    """
+    <style>
+    /* Adjust the sidebar position */
+    .sidebar .sidebar-content {
+        position: fixed;
+        overflow-y: hidden; /* Hide vertical scrollbar */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+# Load Data (replace 'ratings.csv' and 'movies.csv' with your actual data files)
+ratings_df = pd.read_csv('ratings.csv')
+movies_data = pd.read_csv('movies.csv')
+original_movies = movies_data
+
 # Function to remove the year from movie titles
 def remove_year(title):
     return re.sub(r'\(\d{4}\)', '', title).strip()
 
-# Load Data
-ratings_df = pd.read_csv('ratings.csv')
-movies_data = pd.read_csv('movies.csv')
-original_movies = movies_data
+# Apply the function to the 'title' column
 original_movies['title'] = original_movies['title'].apply(remove_year)
 
 # Collaborative Filtering Setup
 reader = Reader(rating_scale=(0.5, 5))
 data = Dataset.load_from_df(ratings_df[['userId', 'movieId', 'rating']], reader)
+# Split the data
 trainset, testset = train_test_split(data, test_size=0.2, random_state=42)
+# Build and train an SVD model
 svd = SVD()
 svd.fit(trainset)
 
@@ -35,52 +53,69 @@ def get_content_based_recommendations(title, r_c=11):
     idx = original_movies.index[original_movies['title'] == title].tolist()[0]
     sim_scores = list(enumerate(cosine_sim[idx]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    # Exclude the movie itself
     sim_scores = sim_scores[1:r_c]
     movie_indices = [i[0] for i in sim_scores]
     return original_movies['title'].iloc[movie_indices]
 
-# Hybrid recommendations function with item-based recommendations
-def hybrid_recommendations(userId, title, k=10, r_c=11):
-    content_based_recs = get_content_based_recommendations(title, r_c)
-    collab_filtering_recs = []
+# Function to generate an explanation for a movie (replace with your code)
+def generate_explanation(movie, movie_title):
+    # Replace this with your explanation generation code
+    return "Explanation for " + movie
 
+# Function to get hybrid recommendations
+def hybrid_recommendations(userId, title, k=10, r_c=11):
+    # Get content-based recommendations
+    content_based_recs = get_content_based_recommendations(title, r_c)
+
+    # Get collaborative filtering recommendations using item-based approach
+    collab_filtering_recs = []
     try:
         movie_id = original_movies['movieId'][original_movies['title'] == title].values[0]
         user_id = userId
+        # Calculate item-item similarity using item ratings (replace with your code)
         item_similarity = cosine_sim.dot(ratings_df.T)
+        # Get similar items to the target movie
         similar_items = item_similarity[movie_id]
+        # Sort by similarity and get the top k
         top_similar_items = similar_items.argsort()[::-1][:k]
         collab_filtering_recs = [original_movies['title'].iloc[i] for i in top_similar_items]
     except:
         pass
 
     recommendations = list(set(content_based_recs) | set(collab_filtering_recs))
+
     return recommendations
 
 # Streamlit interface
-def main():
-    st.title("Movie Recommendation System")
-    user_id = st.text_input("Select User ID:")
-    movie_title = st.text_input("Enter Movie Title:")
-    num_recommendations = st.text_input("Number of Recommendations:")
+st.title("Movie Recommendation System")
+user_id = st.text_input("Select User ID:", )
+movie_title = st.text_input("Enter Movie Title:")
+num_recommendations = st.text_input("Number of Recommendations:", )
 
-    if st.button("Get Recommendations"):
-        recommendations = hybrid_recommendations(user_id, movie_title, num_recommendations)
-        st.subheader("Recommended Movies:")
+if st.button("Get Recommendations"):
+    recommendations = hybrid_recommendations(user_id, movie_title, num_recommendations)
 
-        for i, movie in enumerate(recommendations):
-            button = st.checkbox(f"Show Explanation for {movie}", False)
-            if button:
-                explanation = generate_explanation(movie, movie_title)  # Generate an explanation
-                st.markdown(
-                    f"""<div style='border: 2px solid #ccc; padding: 10px; margin: 5px;'>{i+1}. {movie}<br>Type of Movie is {movie_genre}<br>Explanation: {explanation}</div>""",
-                    unsafe_allow_html=True
-                )
+    st.subheader("Recommended Movies:")
+    for i, movie in enumerate(recommendations):
+        # Create a button to show/hide the explanation
+        button = st.checkbox(f"Show Explanation for {movie}", False)
+        if button:
+            explanation = generate_explanation(movie, movie_title)  # Generate an explanation
+            st.markdown(
+                f"""<div style='border: 2px solid #ccc; padding: 10px; margin: 5px;'>{i+1}. {movie}<br>Type of Movie is {movie_genre}<br>Explanation: {explanation}</div>""",
+                unsafe_allow_html=True
+            )
 
-# Function to generate an explanation
-def generate_explanation(movie_title, user_preferences):
-    explanation = f"We recommend '{movie_title}' because it matches your preference for {user_preferences} and It belongs to the genre {original_movies[original_movies['title'] == movie_title]['genres'].values[0]}."
-    return explanation
+# Add your image URLs and captions here
+image_url1 = "https://media.comicbook.com/2019/03/toy-story-4-poster-1163565.jpeg"
+caption1 = "Toy Story"
+image_url2 = "https://media.wdwnt.com/2019/12/EK9zDtnUcAA5zXM-4.jpeg"
+caption2 = "Mulan"
+image_url3 = "https://intheposter.com/cdn/shop/products/the-family-comedy-in-the-poster-1_1200x.jpg?v=1694762497"
+caption3 = "The Johnsons"
 
-if __name__ == "__main__":
-    main()
+# Display images in the sidebar
+st.sidebar.image(image_url1, caption=caption1, use_column_width=True)
+st.sidebar.image(image_url2, caption=caption2, use_column_width=True)
+st.sidebar.image(image_url3, caption=caption3, use_column_width=True)
